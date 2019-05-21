3f378fb463829e3d968a73225608fd4b682a3b84
emily <mle4d@me.com>
2019-05-20 22:27:46 -0700

    list data

---
+++ src/list.js
@@ -0,0 +1,24 @@
+const list = [
+    {
+        item: 'chikn',
+        completed: true
+    },
+    {
+        item: 'peas & carrots',
+        completed: false
+    },
+    {
+        item: 'flour',
+        completed: true
+    },
+    {
+        item: 'gravy',
+        completed: false
+    },
+    {
+        item: 'earth balance',
+        completed: false
+    },
+];
+
+export default list;
